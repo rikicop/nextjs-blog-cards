367eccd346cd07f4b3534c14f3a6a78f50a6c966
rikicop <ruperto1@protonmail.com>
2022-04-12 21:29:13 -0500

    first

---
+++ Interfaces.ts
@@ -0,0 +1,29 @@
+export interface IndexPageProps {
+  data: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+    excerpt: string;
+  }[];
+}
+
+export interface ArticleProps {
+  article: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+    excerpt: string;
+  };
+}
+
+export interface ArticleItemProps {
+  data: {
+    id: number;
+    title: string;
+    picture?: string | any;
+    body: string;
+    excerpt: string;
+  };
+}
